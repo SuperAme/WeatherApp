e56e2989a8d2b744ff49870f78f70fedcdc3bc78
Americo Meneses <beatamez@hotmail.com>
2023-01-18 17:47:01 -0600

    Creating groups for files and adding api extension files

--- WeatherAppProject/Networking/API.swift
+++ WeatherAppProject/Networking/API.swift
@@ -1,8 +1,12 @@
 //
-//  Api.swift
+//  API.swift
 //  WeatherAppProject
 //
 //  Created by Américo MQ on 18/01/23.
 //
 
 import Foundation
+
+struct API {
+    static let key = "6deb54b7826625462379de7db3d85c43"
+}
