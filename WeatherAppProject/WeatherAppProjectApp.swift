27bb5747b660bb4c39f5b5d153284e2fdf63d284
Americo Meneses <beatamez@hotmail.com>
2023-01-18 17:17:21 -0600

    Initial Commit

---
+++ WeatherAppProject/WeatherAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppProjectApp.swift
+//  WeatherAppProject
+//
+//  Created by Américo MQ on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
